88c48cf1500c7697d924dd50abbb5e64fffba7e8
kj-ka <keshavkaushal787@gmail.com>
2019-01-26 19:27:50 +0000

    Updates

---
+++ precache-manifest.f79ae2285222d264a0672e2a28b9123c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "604da090b5f8560b2f43",
+    "url": "/graph2/static/js/runtime~main.604da090.js"
+  },
+  {
+    "revision": "ff665f4b867e69c37514",
+    "url": "/graph2/static/js/main.ff665f4b.chunk.js"
+  },
+  {
+    "revision": "c72e398db403ec1bc98a",
+    "url": "/graph2/static/js/1.c72e398d.chunk.js"
+  },
+  {
+    "revision": "ff665f4b867e69c37514",
+    "url": "/graph2/static/css/main.b6881d5b.chunk.css"
+  },
+  {
+    "revision": "a1ea9b2cba5debed1373981333769e50",
+    "url": "/graph2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/graph2/precache-manifest.30ec2f7e510cc6d286ce6ff235f6ef36.js"
+  "/graph2/precache-manifest.f79ae2285222d264a0672e2a28b9123c.js"
 );
 
 workbox.clientsClaim();
