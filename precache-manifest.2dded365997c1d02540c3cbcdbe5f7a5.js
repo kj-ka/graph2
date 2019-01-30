b9f4ef129439502451074764b9f9afa89fd77dc1
kj-ka <keshavkaushal787@gmail.com>
2019-01-30 12:00:59 +0000

    Updates

--- precache-manifest.2dded365997c1d02540c3cbcdbe5f7a5.js
+++ precache-manifest.2dded365997c1d02540c3cbcdbe5f7a5.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/graph2/static/js/runtime~main.604da090.js"
   },
   {
-    "revision": "c7de1f727a5684b7ed6d",
-    "url": "/graph2/static/js/main.c7de1f72.chunk.js"
+    "revision": "2cf8aeb64ee53466efb0",
+    "url": "/graph2/static/js/main.2cf8aeb6.chunk.js"
   },
   {
     "revision": "878515eeb092345649a3",
     "url": "/graph2/static/js/1.878515ee.chunk.js"
   },
   {
-    "revision": "c7de1f727a5684b7ed6d",
-    "url": "/graph2/static/css/main.3282ce58.chunk.css"
+    "revision": "2cf8aeb64ee53466efb0",
+    "url": "/graph2/static/css/main.430be5cf.chunk.css"
   },
   {
-    "revision": "482b187ef372085ee843b8558e11e310",
+    "revision": "8cb61058c5a2480b87d0ce6c775e3a00",
     "url": "/graph2/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/graph2/precache-manifest.8e837b85e9793062826a371629fb544a.js"
+  "/graph2/precache-manifest.2dded365997c1d02540c3cbcdbe5f7a5.js"
 );
 
 workbox.clientsClaim();
